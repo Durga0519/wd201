47957ee96dd559af3f415cb87062f4c93d7096f0
DSOLVE <Learngit19@email.com>
2024-09-25 13:10:07 +0530

    http-server connected with registration page

--- hello-world/index.js
+++ hello-world/index.js
@@ -1,5 +1,18 @@
-const hello = () =>{
-    console.log("Hello GitHub!");
-};
+const http=require("http");
+const fs = require("fs");
 
-hello();
+/*fs.writeFile(
+    "sample.txt",
+    "Hello World. Welcome to Node.js File System module.",
+    (err) => {
+      if (err) throw err;
+      console.log("File created!");
+    }
+  );*/
+
+const server=http.createServer((req,res) =>{
+    const stream=fs.createReadStream("sample.txt");
+    stream.pipe(res);
+})
+
+server.listen(3000)
